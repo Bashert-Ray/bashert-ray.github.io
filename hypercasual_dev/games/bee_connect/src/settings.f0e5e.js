0ed6f4c0b0f71d93f0063c0e6d7c0f01de263b1b
Bashert <greataxe.ray@gmail.com>
2020-12-28 10:00:15 +0800

    update

---
+++ hypercasual_dev/games/bee_connect/src/settings.f0e5e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/mainGame.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"e6f8e",main:"6c625"}};
