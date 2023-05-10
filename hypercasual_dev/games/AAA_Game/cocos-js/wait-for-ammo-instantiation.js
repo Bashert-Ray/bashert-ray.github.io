403dfca5c645e7bab0bf5c321b7f3371a33713bc
Bashert <greataxe.ray@gmail.com>
2023-05-10 18:34:24 +0800

    Updated

--- hypercasual_dev/games/AAA_Game/cocos-js/wait-for-ammo-instantiation.js
+++ hypercasual_dev/games/AAA_Game/cocos-js/wait-for-ammo-instantiation.js
@@ -1,1 +1,1 @@
-System.register(["./instantiated-0b40bde4.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hY)}],execute:function(){}}}));
+System.register(["./instantiated-ada81b78.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hY)}],execute:function(){}}}));
